d0aedb45a02d601bfc890324910e9309e8c2437c
bokunohaven <afonsojacobamorim@gmail.com>
2025-12-14 15:58:08 +0000

    Correção no erro de não conseguir entrar na pagina home do professor.

--- src/main/java/pt/up/edscrum/model/User.java
+++ src/main/java/pt/up/edscrum/model/User.java
@@ -2,6 +2,8 @@ package pt.up.edscrum.model;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,7 +32,9 @@ public class User {
 
     private String profileImage;
 
+    @JsonIgnore
     private String resetCode;
+    @JsonIgnore
     private LocalDateTime resetCodeExpiry;
 
     // Getters e Setters
